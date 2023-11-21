4256cb7fd9359ce624c776e8eae1d0cd378bfc23
akheel-zuber <127199538+akheel-zuber@users.noreply.github.com>
2023-11-21 23:47:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,18 +3,50 @@ document.addEventListener('DOMContentLoaded', function () {
   const entriesTable = document.getElementById('entriesTable');
   const entriesTableBody = entriesTable.getElementsByTagName('tbody')[0];
 
+  function saveToLocalStorage(entry) {
+    let entries = JSON.parse(localStorage.getItem('entries')) || [];
+    entries.push(entry);
+    localStorage.setItem('entries', JSON.stringify(entries));
+  }
 
-  loadEntriesFromStorage();
+ 
+  function loadFromLocalStorage() {
+    let entries = JSON.parse(localStorage.getItem('entries')) || [];
+    for (const entry of entries) {
+      addEntryToTable(entry);
+    }
+  }
+
+  function addEntryToTable(entry) {
+    const newRow = entriesTableBody.insertRow();
+    const cell1 = newRow.insertCell(0);
+    const cell2 = newRow.insertCell(1);
+    const cell3 = newRow.insertCell(2);
+    const cell4 = newRow.insertCell(3);
+    const cell5 = newRow.insertCell(4);
+
+    
+    cell1.textContent = entry.name;
+    cell2.textContent = entry.email;
+    cell3.textContent = entry.password;
+    cell4.textContent = entry.dob;
+    cell5.textContent = entry.terms ? 'Yes' : 'No';
+  }
+
+ 
+  loadFromLocalStorage();
 
   form.addEventListener('submit', function (event) {
     event.preventDefault();
+
+  
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
     const dob = document.getElementById('dob').value;
     const terms = document.getElementById('terms').checked;
 
-  
+    
     const dobDate = new Date(dob);
     const today = new Date();
     const minDate = new Date(today.getFullYear() - 55, today.getMonth(), today.getDate());
@@ -25,62 +57,11 @@ document.addEventListener('DOMContentLoaded', function () {
       return;
     }
 
-    const newRow = entriesTableBody.insertRow();
-    const cell1 = newRow.insertCell(0);
-    const cell2 = newRow.insertCell(1);
-    const cell3 = newRow.insertCell(2);
-    const cell4 = newRow.insertCell(3);
-    const cell5 = newRow.insertCell(4);
+   
+    saveToLocalStorage({ name, email, password, dob, terms });
+    addEntryToTable({ name, email, password, dob, terms });
 
- 
-    cell1.textContent = name;
-    cell2.textContent = email;
-    cell3.textContent = password;
-    cell4.textContent = dob;
-    cell5.textContent = terms ? 'Yes' : 'No';
-
-
-    saveEntryToStorage(name, email, password, dob, terms);
 
     form.reset();
   });
-
-  function saveEntryToStorage(name, email, password, dob, terms) {
-
-    const existingEntries = JSON.parse(localStorage.getItem('entries')) || [];
-
-
-    const newEntry = { name, email, password, dob, terms };
-    existingEntries.push(newEntry);
-
-
-    localStorage.setItem('entries', JSON.stringify(existingEntries));
-
-
-    loadEntriesFromStorage();
-  }
-
-  function loadEntriesFromStorage() {
-
-    const entries = JSON.parse(localStorage.getItem('entries')) || [];
-
-    entriesTableBody.innerHTML = '';
-
-
-    entries.forEach(entry => {
-      const newRow = entriesTableBody.insertRow();
-      const cell1 = newRow.insertCell(0);
-      const cell2 = newRow.insertCell(1);
-      const cell3 = newRow.insertCell(2);
-      const cell4 = newRow.insertCell(3);
-      const cell5 = newRow.insertCell(4);
-
-      cell1.textContent = entry.name;
-      cell2.textContent = entry.email;
-      cell3.textContent = entry.password;
-      cell4.textContent = entry.dob;
-      cell5.textContent = entry.terms ? 'Yes' : 'No';
-    });
-  }
 });
-
